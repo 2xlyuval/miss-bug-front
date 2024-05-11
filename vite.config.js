e58c669341b4c90592cae1b623ad590525133b64
yuvalpwix <yuvalpi@wix.com>
2024-05-11 16:27:45 +0300

    update vite-config -build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //if the backend is in the same project so the build should be in the backend public folder
+  // build: {
+  //   outDir: '..backend/public',
+  //   emptyOutDir: true,
+  // },
 })
